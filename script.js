5c83894c4fc33b3a9013f1ea95947dd96b674f23
stevekindstrand <stevekindstrand@gmail.com>
2021-12-07 17:02:05 +0100

    done

---
+++ script.js
@@ -0,0 +1,19 @@
+let input = document.getElementById("input");
+let button = document.getElementById("button");
+let myDiv = document.getElementById("myDiv");
+
+button.addEventListener("click", function(){
+    let test = input.value * 365 * 24
+    myDiv.innerHTML = "<br>" + input.value + " år = " + test + " timmar"
+});
+
+// function hours(){
+//     let number = 1;
+//     return number * 365 * 24;
+// };
+
+// myDiv.innerText = hours();
+
+
+
+
